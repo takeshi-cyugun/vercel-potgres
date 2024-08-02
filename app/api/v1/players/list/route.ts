bf1e94fb99d63362a1452b80c41fc5de339286e8
takeshi-cyugun <t.cyugun@gmail.com>
2024-08-02 16:43:14 +0900

    選手情報全件取得スイッチ

--- app/api/v1/players/list/route.ts
+++ app/api/v1/players/list/route.ts
@@ -7,64 +7,129 @@ export async function GET(request: Request) {
   const match_id = searchParams.get('match_id');
   const team_id = searchParams.get('team_id');
 
-  const { rows, rowCount } = await sql`
-    select * from (
-      select 
-        t.id          as team_id
-      , t.name        as team_name
-      , p.id          as player_id
-      , p.grade       as grade
-      , p.feature     as feature 
-      , p.clothing    as clothing
-      , p.hairstyle   as hairstyle
-      , p.shoes       as shoes
-      , p.nickname    as nickname
-      , p.techniques  as techniques 
-      , p.strong_weak as strong_weak
-      , p.level       as level
-      , p.win         as win
-      , m.strategyA   as strategy
-      , 'A'           as position
-    from matches m
-      inner join teams t
-      on m.teama_id = t.id
-      inner join players p
-      on p.team_id = t.id
-      and m.id = p.match_id
-      where t.id = ${team_id}
-      and m.id = ${match_id}
+  if (match_id) {
 
-    union 
-      
-      select 
-        t.id          as team_id
-      , t.name        as team_name
-      , p.id          as player_id
-      , p.grade       as grade
-      , p.feature     as feature
-      , p.clothing    as clothing
-      , p.hairstyle   as hairstyle
-      , p.shoes       as shoes
-      , p.nickname    as nickname
-      , p.techniques  as techniques 
-      , p.strong_weak as strong_weak
-      , p.level       as level
-      , p.win         as win
-      , m.strategyB   as strategy
-      , 'B'           as position
-    from matches m
-      inner join teams t
-      on m.teamb_id = t.id
-      inner join players p
-      on p.team_id = t.id
-      and m.id = p.match_id
-      where t.id = ${team_id}
-      and m.id = ${match_id}
-    ) A
-    order by grade, player_id
-  `;
+    const { rows, rowCount } = await sql`
+      select * from (
+        select 
+          t.id          as team_id
+        , t.name        as team_name
+        , p.id          as player_id
+        , p.grade       as grade
+        , p.feature     as feature 
+        , p.clothing    as clothing
+        , p.hairstyle   as hairstyle
+        , p.shoes       as shoes
+        , p.nickname    as nickname
+        , p.techniques  as techniques 
+        , p.strong_weak as strong_weak
+        , p.level       as level
+        , p.win         as win
+        , m.strategyA   as strategy
+        , 'A'           as position
+      from matches m
+        inner join teams t
+        on m.teama_id = t.id
+        inner join players p
+        on p.team_id = t.id
+        and m.id = p.match_id
+        where t.id = ${team_id}
+        and m.id = ${match_id}
 
-  return NextResponse.json( rows , { status: 200 });
+      union 
+        
+        select 
+          t.id          as team_id
+        , t.name        as team_name
+        , p.id          as player_id
+        , p.grade       as grade
+        , p.feature     as feature
+        , p.clothing    as clothing
+        , p.hairstyle   as hairstyle
+        , p.shoes       as shoes
+        , p.nickname    as nickname
+        , p.techniques  as techniques 
+        , p.strong_weak as strong_weak
+        , p.level       as level
+        , p.win         as win
+        , m.strategyB   as strategy
+        , 'B'           as position
+      from matches m
+        inner join teams t
+        on m.teamb_id = t.id
+        inner join players p
+        on p.team_id = t.id
+        and m.id = p.match_id
+        where t.id = ${team_id}
+        and m.id = ${match_id}
+      ) A
+      order by grade, player_id
+    `;
+
+    return NextResponse.json( rows , { status: 200 });
+  } else {
+
+
+    const { rows, rowCount } = await sql`
+      select * from (
+        select 
+          t.id          as team_id
+        , t.name        as team_name
+        , p.id          as player_id
+        , p.grade       as grade
+        , p.feature     as feature 
+        , p.clothing    as clothing
+        , p.hairstyle   as hairstyle
+        , p.shoes       as shoes
+        , p.nickname    as nickname
+        , p.techniques  as techniques 
+        , p.strong_weak as strong_weak
+        , p.level       as level
+        , p.win         as win
+        , m.strategyA   as strategy
+        , 'A'           as position
+      from matches m
+        inner join teams t
+        on m.teama_id = t.id
+        inner join players p
+        on p.team_id = t.id
+        and m.id = p.match_id
+        where t.id = ${team_id}
+
+      union 
+        
+        select 
+          t.id          as team_id
+        , t.name        as team_name
+        , p.id          as player_id
+        , p.grade       as grade
+        , p.feature     as feature
+        , p.clothing    as clothing
+        , p.hairstyle   as hairstyle
+        , p.shoes       as shoes
+        , p.nickname    as nickname
+        , p.techniques  as techniques 
+        , p.strong_weak as strong_weak
+        , p.level       as level
+        , p.win         as win
+        , m.strategyB   as strategy
+        , 'B'           as position
+      from matches m
+        inner join teams t
+        on m.teamb_id = t.id
+        inner join players p
+        on p.team_id = t.id
+        and m.id = p.match_id
+        where t.id = ${team_id}
+      ) A
+      order by grade, player_id
+    `;
+
+    return NextResponse.json( rows , { status: 200 });
+
+  }
+
+
 }
 
 
