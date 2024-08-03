60edfaebdfcf519c7593bb98362d5346578fb61c
takeshi-cyugun <t.cyugun@gmail.com>
2024-08-03 12:37:50 +0900

    アイコン調整

--- app/api/v1/events/list/route.ts
+++ app/api/v1/events/list/route.ts
@@ -4,6 +4,6 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const season = searchParams.get('season');
-  const { rows, rowCount } = (season) ? await sql`SELECT * FROM events where season = ${season};` : await sql`SELECT * FROM events;`
+  const { rows, rowCount } = (season) ? await sql`SELECT * FROM events where season = ${season} ORDER BY ABS(current_date - event_date);` : await sql`SELECT * FROM events ORDER BY ABS(current_date - event_date);`
   return NextResponse.json( rows , { status: 200 });
 }
